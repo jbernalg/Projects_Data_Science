6d1a0917cfcbfd91ad564848171b237887bed5ec
jbernalg <jeinffersonbernal@gmail.com>
2024-08-08 11:27:02 -0500

    Reto 2.3

---
+++ Course_ML/Reto_02/Variante1_3.py
@@ -0,0 +1,16 @@
+'''
+Nombre: Jeinfferson Bernal
+C.I: 4606424
+Correo: jeinffersonbernal@gmail.com
+'''
+
+'''
+	3.- Escribe un programa que convierta grados Celsius a grados Fahrenheit 
+    sabiendo que (0°Cx9/5)+32= 32 °F y que imprima en pantalla el resultado:
+    Input: Grados Celsius
+    Output: Grados Fahrenheit
+'''
+
+celcius = float(input('Ingrese Grados Celcius: '))
+fahr = celcius*(9/5) + 32
+print(f'{fahr} Grados Fahrenheit')
