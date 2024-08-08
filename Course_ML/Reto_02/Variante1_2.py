11de0a6ca9517b247ac9d08b8fbc7f71aaaec89e
jbernalg <jeinffersonbernal@gmail.com>
2024-08-08 11:19:37 -0500

    Reto 2.2

---
+++ Course_ML/Reto_02/Variante1_2.py
@@ -0,0 +1,13 @@
+'''
+Nombre: Jeinfferson Bernal
+C.I: 4606424
+Correo: jeinffersonbernal@gmail.com
+'''
+
+'''
+2.	Escribe un programa en Python que pregunte el nombre al usuario y luego imprima 
+en pantalla un saludo al usuario de la siguiente forma: ¡Hola (Nombre)!
+'''
+
+Nombre = input('Como es tu nombre?: ')
+print(f'¡Hola {Nombre}!')
