0611c5c72111fa1909e3e0c2de07218c1ec02b69
jbernalg <jeinffersonbernal@gmail.com>
2024-08-08 11:16:42 -0500

    Reto 2.1

---
+++ Course_ML/Reto_02/Variante1_1.py
@@ -0,0 +1,14 @@
+'''
+Nombre: Jeinfferson Bernal
+C.I: 4606424
+Correo: jeinffersonbernal@gmail.com
+'''
+
+'''
+1.	Escribe un programa en Python que almacene la frase "¡Hola mundo!” en una variable 
+y que luego imprima en pantalla la frase almacenada
+'''
+
+
+message = '¡Hola mundo!'
+print(message)
