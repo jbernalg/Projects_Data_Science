168a34426d3106415baff605a80605865a0f80d0
jbernalg <jeinffersonbernal@gmail.com>
2024-08-08 11:37:26 -0500

    Reto 2.5

---
+++ Course_ML/Reto_02/Variante1_5.py
@@ -0,0 +1,29 @@
+'''
+Nombre: Jeinfferson Bernal
+C.I: 4606424
+Correo: jeinffersonbernal@gmail.com
+'''
+
+'''
+5.	En Python, asigna a las variables v y f los datos booleanos True y False 
+respectivamente y luego imprime en pantalla las siguientes comparaciones:
+
+-v or f
+-v and f
+-not v
+-not f
+
+Saca tus propias conclusiones sobre los resultados mostrados
+'''
+
+v = True
+f = False
+
+print(v or f)
+print(v and f)
+print(not v)
+print(not f)
+
+'''
+Se trata sobre los operadores logicos y permiten comprobar condiciones
+'''
