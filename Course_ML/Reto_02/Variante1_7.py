497696a821967ad19c43b60e3906aad3dccab2e9
jbernalg <jeinffersonbernal@gmail.com>
2024-08-08 11:43:58 -0500

    Reto 2.7

---
+++ Course_ML/Reto_02/Variante1_7.py
@@ -0,0 +1,16 @@
+'''
+Nombre: Jeinfferson Bernal
+C.I: 4606424
+Correo: jeinffersonbernal@gmail.com
+'''
+
+'''
+    7.	Escribe un programa que imprima en pantalla el largo de una cadena de texto:
+
+    Input: Ingrese una cadena de texto: ¡Hola mundo!
+    Output: La cadena de texto tiene 12 caracteres
+
+'''
+
+texto = input('Ingrese una cadena de texto: ')
+print(f'La cadena de texto tiene {len(texto)} caracteres')
