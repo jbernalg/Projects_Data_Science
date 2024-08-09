36673ae79dee5e82028aebf99a34a674456cb18b
jbernalg <jeinffersonbernal@gmail.com>
2024-08-09 09:28:35 -0500

    variante 2.5

---
+++ Course_ML/Reto_02/Variante2_5.py
@@ -0,0 +1,11 @@
+'''
+    5.	Escribe un programa que pregunte por las horas trabajadas en el día y 
+    luego calcule el valor de su salario en ese día sabiendo que el costo de la 
+    hora de trabajo es de 7.500 $:
+
+    Input: ¿Cuántas horas trabajó hoy? 8
+    Output: Su salario el día de hoy es de 60.000 $
+
+'''
+n_horas = int(input('¿Cuántas horas trabajó hoy?: '))
+print(f'Su salario el día de hoy es de {n_horas*7500} $')
