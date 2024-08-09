7149eac1d1dfe904fecff260ae56aed63a7e3151
jbernalg <jeinffersonbernal@gmail.com>
2024-08-09 09:25:44 -0500

    variante 2.3

---
+++ Course_ML/Reto_02/variante2_3.py
@@ -0,0 +1,8 @@
+'''
+    3.	Utilizando Python, dado un número n calcula la siguiente operación: (n^2 + 2n)/(2)
+    e imprime en pantalla el resultado
+    Output: El resultado de la operación es
+'''
+
+n = int(input('Ingrese un numero entero: '))
+print(f'El resultado de la operación es {(n**2 + 2*n)/2}')
