fd5c62278ce42ca9e8725558c5e30ac879d83f87
jbernalg <jeinffersonbernal@gmail.com>
2024-08-09 09:30:58 -0500

    variante 2.6

---
+++ Course_ML/Reto_02/Variante2_6.py
@@ -0,0 +1,10 @@
+'''
+    6.	Escribe un programa que imprima en pantalla el largo de una cadena de texto:
+
+    Input: Ingrese una cadena de texto: ¡Hola mundo!
+    Output: La cadena de texto tiene 12 caracteres
+
+'''
+
+texto = input('Ingrese una cadena de texto: ')
+print(f'La cadena de texto tiene {len(texto)} caracteres')
