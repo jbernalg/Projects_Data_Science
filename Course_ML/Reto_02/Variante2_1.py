d0dfdb694ff4e6ae0c74b8d6aa347a85bb7c6664
jbernalg <jeinffersonbernal@gmail.com>
2024-08-09 09:16:19 -0500

    variante 2.1

---
+++ Course_ML/Reto_02/Variante2_1.py
@@ -0,0 +1,20 @@
+'''
+    1.	En Python, crea una variable por cada letra de tu nombre y asigna a la variable la 
+    letra de tu nombre que corresponde y que luego imprima en pantalla cada letra del 
+    nombre y finalmente el nombre entero:
+
+    Output: P e d r o
+    Pedro
+'''
+
+J = 'J'
+e = 'e'
+i = 'i'
+n = 'n'
+f = 'f'
+r = 'r'
+s = 's'
+o = 'o'
+
+print(J,e,i,n,f,r,s,o)
+print(J,e,i,n,f,f,e,r,s,o,n)
