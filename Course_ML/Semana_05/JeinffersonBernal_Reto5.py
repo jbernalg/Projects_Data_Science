91e2ebb882a4933b8a3f09d528e882ddfed42edb
jbernalg <jeinffersonbernal@gmail.com>
2024-09-17 15:31:02 -0500

    Reto 5 inicio

---
+++ Course_ML/Semana_05/JeinffersonBernal_Reto5.py
@@ -0,0 +1,16 @@
+'''
+Administrador de Notas
+'''
+
+BD_estudiantes = {123: {'Nombre': 'Jose',
+                        'Correo': 'xxxx',
+                        'Telefono': 2033334,
+                        'Fecha_Nacimiento': '30-11',
+                        'Nota1': 12,
+                        'Nota2': 18,
+                        'nota3': 16,
+                        'nota4': 20
+                        }}
+
+print(BD_estudiantes[123]['Nombre'])
+
