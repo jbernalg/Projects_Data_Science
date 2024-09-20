6313ce33826bc5503484edd6e6eb49e710acd931
jbernalg <jeinffersonbernal@gmail.com>
2024-09-20 07:56:29 -0500

    practica librerias y modulo

---
+++ Course_ML/Semana_06/MiModulo.py
@@ -0,0 +1,13 @@
+'''
+#MiModulo.py
+Vamos a definir dos funciones aritmeticas
+'''
+
+def suma(a,b):
+    '''Suma de dos numeros'''
+    return a+ b
+
+
+def resta(a,b):
+    '''Resta de dos numeros'''
+    return a - b
---
+++ Course_ML/Semana_06/Prueba_Modulo.py
@@ -0,0 +1,9 @@
+'''
+Traer las funciones de MiModulo.py
+y operar con ellas
+'''
+
+import MiModulo
+
+print(MiModulo.suma(10, 23))
+print(MiModulo.resta(9,4))
