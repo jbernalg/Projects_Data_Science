6b3797281a8ce1c05cca8230447488eeec96f2ff
jbernalg <jeinffersonbernal@gmail.com>
2025-05-17 08:10:53 -0500

    normalizar los datos y dividirlos

--- ML/Scikit-Learn/pca.py
+++ ML/Scikit-Learn/pca.py
@@ -20,3 +20,14 @@ if __name__ == '__main__':
     # cargar dataset
     df_heart = pd.read_csv('./data/heart_bd.csv')
     print(df_heart.head(5))
+
+    # dividir los datos en features y target
+    features = df_heart.drop(['target'], axis=1)
+    target = df_heart['target']
+
+    # normalizar los datos es vital al usar PCA
+    features = StandardScaler().fit_transform(features)
+    
+    # dividir los datos en entrenamiento y prueba
+    X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.3, random_state=42)
+    
