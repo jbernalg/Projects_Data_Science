0b149da1ce514855c76bf7b2d1cb445253d3e8e2
jbernalg <jeinffersonbernal@gmail.com>
2025-05-23 07:40:13 -0500

    implementando minibatchkmeans

--- ML/Scikit-Learn/k_means.py
+++ ML/Scikit-Learn/k_means.py
@@ -7,3 +7,21 @@ if __name__ == '__main__':
 
     df = pd.read_csv('./data/candy.csv')
     print(df.head())
+
+    # guardar las variables excepto el nombre del caramelo
+    X = df.drop('competitorname', axis=1)
+
+    # definir y entrenar modelo de clusterizacion. 
+    # Usamos k=4 elegido a nuestra conveniencia
+    # elegimos un tamano de batch de 8
+    kmeans = MiniBatchKMeans(n_clusters=4, batch_size=8).fit(X)
+
+    # mostrar total de clusteres
+    print('Total de clusteres: ', len(kmeans.cluster_centers_))
+    print('='*64)
+    # mostrar las etiquetas 
+    print(kmeans.predict(X))
+
+    # agregar las etiquetas dadas por el modelo al dataframe
+    df['KMeans'] = kmeans.predict(X)
+    print(df.head())
