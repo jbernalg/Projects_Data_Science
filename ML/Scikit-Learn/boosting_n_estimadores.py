5116e2121d0b277df9b5e83c60f5141305dafd13
jbernalg <jeinffersonbernal@gmail.com>
2025-05-23 07:10:00 -0500

    encontrando el mejor numero de estimadores de Boosting 1.2

--- ML/Scikit-Learn/boosting_n_estimadores.py
+++ ML/Scikit-Learn/boosting_n_estimadores.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 # librerias de boosting
 from sklearn.ensemble import GradientBoostingClassifier
@@ -25,4 +27,28 @@ if __name__ == '__main__':
     # definir lista para almacenar accuracy de cada cantidad diferente de estimadores
     total_accuracy = []
 
+    # iteramos sobre las diferentes cantidades de estimadores
+    for i in estimadores:
+        boots = GradientBoostingClassifier(n_estimators=i).fit(X_train, y_train)
+        pred_boots = boots.predict(X_test)
+        total_accuracy.append(accuracy_score(y_test, pred_boots))
+
+    # visualizar los accuracy de cada cantidad de estimador
+    plt.plot(estimadores, total_accuracy)
+    plt.xlabel('Estimadores')
+    plt.ylabel('Accuracy')
+    plt.show()
     
+    # encontrar el mejor accuracy
+    max_accu = max(total_accuracy)
+
+    # encontrar los índices donde se logra ese accuracy
+    best_index = [i for i, v in enumerate(total_accuracy) if v == max_accu]
+
+    # traducir esos índices a los valores de n_estimators reales
+    best_estimators = [estimadores[i] for i in best_index]
+
+    # imprimir resultados
+    print(f"Mejor accuracy: {max_accu}")
+    print(f"Cantidad de estimadores con el mejor accuracy: {len(best_estimators)}")
+    print(f"Estimadores con mejor accuracy: {best_estimators}")
