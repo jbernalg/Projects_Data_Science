c5c8f0845f28d6489ad21ee9d5141bebef8a7c53
jbernalg <jeinffersonbernal@gmail.com>
2025-05-24 06:20:42 -0500

    definir y entrenar modelo Mean Shift

--- ML/Scikit-Learn/meanshift.py
+++ ML/Scikit-Learn/meanshift.py
@@ -6,3 +6,12 @@ if __name__ == '__main__':
 
     dataset = pd.read_csv('./data/candy.csv')
     print(dataset.head())
+
+    # seleccionamos los features
+    X = dataset.drop('competitorname', axis=1)
+
+    # definimos el modelo
+    meanshift = MeanShift().fit(X)
+
+    # mostrar etiquetas creadas
+    print(max(meanshift.labels_))
