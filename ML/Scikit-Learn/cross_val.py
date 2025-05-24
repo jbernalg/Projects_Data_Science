9aa0b55b596066b3caee346955bdeb8c97e6bbb6
jbernalg <jeinffersonbernal@gmail.com>
2025-05-24 07:19:55 -0500

    aplicando una validacion cruzada rapida

--- ML/Scikit-Learn/cross_val.py
+++ ML/Scikit-Learn/cross_val.py
@@ -9,3 +9,16 @@ if __name__ == '__main__':
 
     dataset = pd.read_csv('./data/felicidad.csv')
     print(dataset.head())
+
+    # Seleccionamos los features y el target
+    X = dataset.drop(['country', 'score'], axis=1)
+    y = dataset['score']
+
+    # Definimos el modelo
+    model = DecisionTreeRegressor()
+
+    # realiza una validacion rapida
+    # scoring: define el metodo a utilizar para hacer el calculo cd CV
+    # por defecto, crea 5 pliegue con los datos
+    score = cross_val_score(model, X, y, cv=5, scoring='neg_mean_squared_error')
+    print(score)
