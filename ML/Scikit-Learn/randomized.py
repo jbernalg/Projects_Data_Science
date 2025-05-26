f5354fe7fd5310d6629320a235b2d55967f887ab
jbernalg <jeinffersonbernal@gmail.com>
2025-05-26 13:45:03 -0500

    mejor estimador - parametros

--- ML/Scikit-Learn/randomized.py
+++ ML/Scikit-Learn/randomized.py
@@ -9,6 +9,10 @@ if __name__ == '__main__':
     dataset = pd.read_csv('./data/felicidad.csv')
     print(dataset.head())
 
+    # saparamos features y target
+    X = dataset.drop(['country', 'rank', 'score'], axis=1)
+    y = dataset['score']
+
     # definir modelo
     reg = RandomForestRegressor()
 
@@ -19,4 +23,17 @@ if __name__ == '__main__':
         'max_depth': range(2, 11) # rango de limitacion del arbol
     }
 
-    
+    # definir optimizador
+    # n_iter: maximo de muestreo. Elige una configuracion de parametros y lo itera
+    # cv: validacion cruzada. Define el numero de pliegue en que se dividen los datos
+    # scoring: metrica para evaluar el modelo
+    rand_est = RandomizedSearchCV(reg, parametros, n_iter=10, cv=3, scoring='neg_mean_absolute_error').fit(X, y)
+
+    # muestra mejor configuracion
+    print(rand_est.best_estimator_)
+    # muestra mejores parametros
+    print(rand_est.best_params_)
+    # obtener prediccion para la primera fila
+    print(rand_est.predict(X.loc[[0]]))
+
+    # valor real de score: 7.5944
