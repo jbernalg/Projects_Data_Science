bf8cd6438e08ab0d0efaa6c6156356b0469227e0
jbernalg <jeinffersonbernal@gmail.com>
2025-05-26 08:57:21 -0500

    entrenar y evaluar modelo para cada pliegue

--- ML/Scikit-Learn/cross_val.py
+++ ML/Scikit-Learn/cross_val.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import cross_val_score, KFold
+from sklearn.metrics import mean_squared_error
 
 # Script principal
 if __name__ == '__main__':
@@ -17,7 +18,7 @@ if __name__ == '__main__':
     # Definimos el modelo
     model = DecisionTreeRegressor()
 
-    # realiza una validacion rapida
+    # realiza una validacion rapida con cross_val_score
     # scoring: define el metodo a utilizar para hacer el calculo cd CV
     # por defecto, crea 5 pliegue con los datos
     score = cross_val_score(model, X, y, cv=5, scoring='neg_mean_squared_error')
@@ -28,3 +29,39 @@ if __name__ == '__main__':
 
     # el score que interpretamos es el valor absoluto de la media anterior
     print(np.abs(np.mean(score)))
+
+    # realiza una validadion con KFold para entender las particiones de los datos
+    # n_split: define la cantidad de pliegues
+    # shuffle: define el orden de los datos
+    kf = KFold(n_splits=3, shuffle=True, random_state=42)
+
+    # lista para almacenar la evaluacion del modelo en los diferentes pliegues
+    mse_values = []
+
+    # mostrar los 3 pliegues de datos generados
+    for train, test in kf.split(dataset):
+        print(train)
+        print(test)
+
+        # dividir cada pliegue en datos de entrenamiento y prueba
+        X_train = X.iloc[train]
+        y_train = y.iloc[train]
+        X_test = X.iloc[test]
+        y_test = y.iloc[test]
+
+        # definir y entrenar el modelo     
+        model = DecisionTreeRegressor().fit(X_train, y_train)
+
+        # obtener las predicciones del modelo
+        predict = model.predict(X_test)
+
+        # evaluar el modelo y guardar evaluacion
+        mse_values.append(mean_squared_error(y_test, predict)) 
+
+    
+    # mostrar metrica de evaluacion de cada modelo
+    print('MSE de cada pliegue: ', mse_values)
+    # mostrar promedio 
+    print('MSE promedio: ', np.mean(mse_values))
+
+        
