9f643bed8888947ac03d2e3f6bafdd850b567aab
jbernalg <jeinffersonbernal@gmail.com>
2025-05-27 08:58:17 -0500

    definir varias funciones de utileria

--- ML/Scikit-Learn/Estructura_Modular_Project/utils.py
+++ ML/Scikit-Learn/Estructura_Modular_Project/utils.py
@@ -2,4 +2,12 @@
 class Utils:
 
     def load_from_csv(self, path):
-        return pd.read_csv()
+        return pd.read_csv(path)
+    
+    def load_from_mysql(self):
+        pass
+
+    def features_target(self, dataset, drop_cols, y):
+        X = dataset.drop(drop_cols, axis=1)
+        y = dataset[y]
+        return X,y
